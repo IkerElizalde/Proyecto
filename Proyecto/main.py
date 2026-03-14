f8e516618e83478ccd4f46a8bbe483cec2806ab9
IkerElizalde <elizalde.iker@correo.unimet.edu.ve>
2026-03-14 13:22:34 -0400

    Update main.py

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,6 +1,25 @@
-from clasesprofmat import *
-from clasehorarios import *
-from apihandler import APIHandler
+from clasehorarios import SistemaHorarios
+
+def menuprincipal()
+  print("-------------------------")
+  print("Sistema de Horarios")
+  print("-------------------------")
+  print("1. Crear listas en blanco")
+  print("2. Descargar los datos de la API en Github")
+  print("3. Cargar un horario en CSV")
+  print("4. Salir del programa")
+
+def modulos()
+  print("-------------------------")
+  print("Modulos")
+  print("-------------------------")
+  print("1. Modulo de Profesores")
+  print("2. Modulo de Materias")
+  print("3. Generacion de Horarios")
+  print("4. Modificacion de Horarios")
+  print("5. Volver al menu principal")
+
+
 
 def main():
 
