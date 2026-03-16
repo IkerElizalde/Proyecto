562aacdb6e2a9f45ecd0de8d4187fd4ecc5b0c8c
IkerElizalde <elizalde.iker@correo.unimet.edu.ve>
2026-03-15 22:36:29 -0400

    Update apihandler.py

--- Proyecto/apihandler.py
+++ Proyecto/apihandler.py
@@ -6,12 +6,38 @@ class APIHandler:
     @staticmethod
     def descargardatos():
         urlprofesores = APIHandler.url + "/profesores.json"
-        urlmaterias = APIHandler.url + "/materias2526-2.json"
-
+        archivosmaterias=["/materias2425-3.json", "/materias2526-1.json", "/materias2526-2.json"]
+    
         try:
             resprofesores = requests.get(urlprofesores)
-            resprofesores.raise_for_status()
+            if resprofesores.status_code!=200:
+                print("Error al descargar profesores")
+                return None, None
+            
             datosprofesores = resprofesores.json()
+
+            datosmaterias=[]
+            codigosvistos=[]
+            for nombrearchivo in archivosmaterias:
+                urlmaterias=APIHandler.url+nombrearchivo
+                reply=requests.get(urlmaterias)
+                if reply.status_code==200:
+                    materiasdelarchivo=respuesta.json()
+                    for m in materiasdelarchivo:
+                        codigoact=m["codigo"]
+                        if codigoact not in codigvistos:
+                            datosmaterias.append(m)
+                            codigosvistos.append(codigo act)
+                else:
+                    print(f"No se pudo acceder a: {nombrearchivo}")
+                    return None, None
+            return datosprofesores, datosmaterias
+
+        except Exception as error:
+            print("\n Hubo un problema de conexion o lectura de datos")
+            print(error)
+            return None, None
+        
 
             respmaterias = requests.get(urlmaterias)
             respmaterias.raise_for_status()
